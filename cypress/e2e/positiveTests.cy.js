7e311b30d17521d30cd1a90487e8c7dc5fe60d22
Moran Liv <moran.liv2006@gmail.com>
2024-11-12 12:21:45 -0800

    Deleted the examples e2e tests from cypress. Added more functions to the test page object.

--- cypress/e2e/positiveTests.cy.js
+++ cypress/e2e/positiveTests.cy.js
@@ -1,6 +1,13 @@
+/*
+Submitted by Moran Liv.
+Email: moran.liv2006@gmail.com
+Phone: 672-963-6730
+*/
+
+import { assert } from "chai";
 import TestPage from "../pageObjects/testPage";
 
-describe("ddd", () => {
+describe("Honeywell BrokerBay Median-Prime-Numbers Cypress Challenge - positive test cases", () => {
 
     const serverUrl = "http://localhost:3000/";
     const testPage = new TestPage;
@@ -13,8 +20,37 @@ describe("ddd", () => {
         testPage.getH1Title().should("be.visible").then((x) => assert.equal(x.text(), "Enter a number to get the median of primes:"))
     })
 
-    it("sdfdfggd", () => {
+    it("Verify median number is presented when typing the smallest prime number that has a median - 3", () => {
+        let value = 3;
 
+        testPage.writeValueInTheInputField(value);
+        testPage.clickOnTheSubmitButton();
+        testPage.getTheResultValue().then((x) => {
+            let returnedValue = (x.text().split(":")[1]).trim();
+            expect(returnedValue).to.contain("2")
+        })
+    })
+
+    it("Verify median number is presented when typing a value that has 2 medians", () => {
+        let value = 4;
+
+        testPage.writeValueInTheInputField(value);
+        testPage.clickOnTheSubmitButton();
+        testPage.getTheResultValue().then((x) => {
+            let returnedValue = (x.text().split(":")[1]).trim();
+            expect(returnedValue).to.contain("2,3")
+        })
+    })
+
+    it("Verify median number is presented when typing 2 digits value", () => {
+        let value = 10;
+
+        testPage.writeValueInTheInputField(value);
+        testPage.clickOnTheSubmitButton();
+        testPage.getTheResultValue().then((x) => {
+            let returnedValue = (x.text().split(":")[1]).trim();
+            expect(returnedValue).to.contain("3,5")
+        })
     })
 
 })
--- cypress/pageObjects/testPage.js
+++ cypress/pageObjects/testPage.js
@@ -3,17 +3,28 @@ class TestPage {
     txt_h1Title = "h1";
     input_numbersInput = "input";
     btn_submitButton = "button";
+    txt_resultText = "h2";
 
     getH1Title() {
         return cy.get(this.txt_h1Title);
     }
 
     writeValueInTheInputField(value) {
-        cy.get(this.input_numbersInput).type(value);
+        cy.get(this.input_numbersInput).type(value).should("have.value", value);
+    }
+
+    getTheNumbersInput() {
+        return cy.get(this.input_numbersInput);
     }
 
     clickOnTheSubmitButton() {
         cy.get(this.btn_submitButton).click();
+    }
+
+    getTheResultValue() {
+        return cy.get(this.txt_resultText).then((x) => {
+            expect(x).to.contain("The median is:")
+        });
     }
 }
 export default TestPage;
